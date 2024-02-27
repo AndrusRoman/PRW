dff08d0b7f7eada341b56d7a94896f320fd6ece7
AndrusRoman <andrus.roman@aluno.ifsp.edu.br>
2024-02-27 12:13:35 -0300

    atualização atividade

--- entrada/exe1.php
+++ entrada/exe1.php
@@ -1,14 +1,25 @@
 <?php
 $nome = $_GET['nome'];
-$email = $_GET['email'];
-$campo_senha = $_GET['campo_senha'];
-$senha = 123;
-
-if($campo_senha==$senha)
-    echo "logado com sucesso";
-else
-    {
-        echo "senha incorreta";
-    }
-
+$email = $_GET['idade'];
+$salario = $_GET['salario'];
+if($salario <= 1412)
+ {
+    $desconto = $salario * 0.075;
+    echo "Desconto INSS = ".number_format($desconto,2,".","");
+ }
+ else if($salario >= 1412.01 && $salario <= 2666.68)
+ {
+    $desconto = $salario * 0.09;
+    echo "Desconto INSS = ".number_format($desconto,2,".","");
+ }
+ else if($salario >= 2666.69 && $salario <= 4000.03)
+ {
+    $desconto = $salario * (12)/100;
+    echo "Desconto INSS = ".number_format($desconto,2,".","");
+ }
+ else if($salario >= 4000.04 && $salario <= 7786.02)
+ {
+    $desconto = $salario * (14)/100;
+    echo "Desconto INSS = ".number_format($desconto,2,".","");
+}
 ?>
